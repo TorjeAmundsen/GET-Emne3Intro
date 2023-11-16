cf7952ed69217bf48ff273674a06c0fe3ab4651e
Torje Amundsen <torjefrafen@hotmail.no>
2023-11-16 10:44:12 +0100

    Initial commit

---
+++ MyConsole.cs
@@ -0,0 +1,12 @@
+﻿namespace TestSpace
+{
+    internal class MyConsole
+    {
+        public static string Ask(string question)
+        {
+            Console.Write(question);
+            var name = Console.ReadLine();
+            return name;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using TestSpace;
+
+namespace Emne3Intro
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = MyConsole.Ask("Input a number: ");
+            try
+            {
+                int number = int.Parse(input);
+                bool isNumberEven = isEven(number);
+                string even = "even!";
+                string odd = "odd!";
+                Console.WriteLine($"The number \"{input}\" is {(isNumberEven ? even : odd)}");
+            }
+            catch (Exception e) {
+                Console.WriteLine(e.Message);
+            }
+        }
+        static bool isEven(int x)
+        {
+            return x % 2 == 0;
+        }
+    }
+}
